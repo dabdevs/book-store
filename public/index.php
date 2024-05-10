96802b7c2a9065fb4ce5e53a6f5607a31179a90a
Alain Jean <ajean@sugit.com.ar>
2024-05-09 22:09:34 -0300

    Routes and index.php entry point

--- public/index.php
+++ public/index.php
@@ -2,3 +2,4 @@
 
 require '../vendor/autoload.php';
 require '../src/Routes/index.php';
+
--- src/Routes/index.php
+++ src/Routes/index.php
@@ -3,10 +3,18 @@
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 use App\Router;
+use App\Database;
+
+try {
+    $db = Database::getInstance("127.0.0.1", "8889", "root", "root", "book_store");
+} catch (\Throwable $th) {
+    throw $th;
+}
 
 $router = new Router();
-
 $router->get('/', HomeController::class, 'index');
-$router->get('/users', UserController::class, 'index');
+$router->post('/users', UserController::class, 'index');
+$router->put('/users', UserController::class, 'update');
+$router->get('/users/delete', UserController::class, 'destroy');
 
 $router->dispatch();
