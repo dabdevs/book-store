4115147d4e89223262f70c234099d3cd94d75f52
Alain Jean <ajean@sugit.com.ar>
2024-05-12 23:43:05 -0300

    User getters and setters

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,10 +8,10 @@ class User
 {
     protected static $instance;
     private $table = "users";
-    private $id;
     public $admin = "ADMIN";
     public $librerian = "LIBRERIAN";
     public $member = "MEMBER";
+    private $id;
     private $firstname;
     private $lastname;
     private $email;
@@ -35,6 +35,9 @@ class User
         return self::$instance;
     }
 
+    /**
+     *  Load input data
+     */
     private function load(array $data)
     {
         if (count($data) > 0) {
@@ -110,5 +113,145 @@ class User
             "birthDate" => $this->birthDate,
             "role" => $this->role,
         ];
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstname
+     */ 
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */ 
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */ 
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of birthDate
+     */ 
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set the value of birthDate
+     *
+     * @return  self
+     */ 
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of role
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
     }
 }
