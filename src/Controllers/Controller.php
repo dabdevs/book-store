e0e3ee22f8ea4b366eff80885e4dcfc20333778c
Alain Jean <ajean@sugit.com.ar>
2024-05-15 17:16:48 -0300

    changed validation function

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -32,10 +32,11 @@ class Controller
         return $data;
     }
 
-    protected function validate(array $data, array $rules)
+    protected function validate(array $rules)
     {
         $table = $rules["table"];
         $errors = [];
+        $data = array_merge($_POST, $_FILES);
         
         if (count($data) > 0) {
             foreach ($data as $key => $value) {
@@ -50,45 +51,45 @@ class Controller
                 // Validate if field is required
                 if (in_array("required", $rule) && $value === "") {
                     $errors[$field] = str_replace("_", " ", $field) . " is required";
-                }
-
-                // Validate if field is a string
-                if (in_array("string", $rule) && !is_string($value)) {
-                    $errors[$field] = str_replace("_", " ", $field) . " must be a string";
-                }
-
-                // Validate if field is a number
-                if (in_array("number", $rule) && !is_numeric($value)) {
-                    $errors[$field] = str_replace("_", " ", $field) . " must be a number";
-                }
-
-                // Validate if field is unique in table
-                if (in_array("$table:unique", $rule)) {
-                    $exists = DB::table($table)->select()->where(str_replace("_", " ", $field) . " = :$field", [":$field" => $value]);
-
-                    if ($exists) $errors[$field] = str_replace("_", " ", $field) . " must be unique";
-                }
-
-                if ($value !== "") {
-                    foreach ($rule as $r) {
-                        // Validate minimum length
-                        if (str_starts_with($r, "min")) {
-                            $length = explode(":", $r)[1];
-                            if (strlen($value) < $length) $errors[$field] = str_replace("_", " ", $field) . " must be at least $length characters";
-                        }
-
-                        // Validate maximum length
-                        if (str_starts_with($r, "max")) {
-                            $length = explode(":", $r)[1];
-                            if (strlen($value) > $length) $errors[$field] = str_replace("_", " ", $field) . " must be at max $length characters";
-                        }
-
-                        // Validate image extension
-                        if (str_starts_with($r, "image")) {
-                            $allowedExtensions = explode(":", $r)[1];
-                            $fileExtension = explode("/", $value["type"])[1];
-                            if (!str_contains($allowedExtensions, $fileExtension)) {
-                                $errors[$field] = str_replace("_", " ", $field) . "'s extension must be " . str_replace(",", " or ", $allowedExtensions);
+                } else {
+                    // Validate if field is a string
+                    if (in_array("string", $rule) && !is_string($value)) {
+                        $errors[$field] = str_replace("_", " ", $field) . " must be a string";
+                    }
+    
+                    // Validate if field is a number
+                    if (in_array("number", $rule) && !is_numeric($value)) {
+                        $errors[$field] = str_replace("_", " ", $field) . " must be a number";
+                    }
+    
+                    // Validate if field is unique in table
+                    if (in_array("$table:unique", $rule)) {
+                        $exists = DB::table($table)->select()->where(str_replace("_", " ", $field) . " = :$field", [":$field" => $value]);
+    
+                        if ($exists) $errors[$field] = str_replace("_", " ", $field) . " must be unique";
+                    }
+    
+                    if ($value !== "") {
+                        foreach ($rule as $r) {
+                            // Validate minimum length
+                            if (str_starts_with($r, "min")) {
+                                $minLength = explode(":", $r)[1];
+                                if (strlen($value) < $minLength) $errors[$field] = str_replace("_", " ", $field) . " must be at least $minLength characters";
+                            }
+    
+                            // Validate maximum length
+                            if (str_starts_with($r, "max")) {
+                                $maxLength = explode(":", $r)[1];
+                                if (strlen($value) > $maxLength) $errors[$field] = str_replace("_", " ", $field) . " must be at max $maxLength characters";
+                            }
+    
+                            // Validate image extension
+                            if (str_starts_with($r, "image")) {
+                                $allowedExtensions = explode(":", $r)[1];
+                                $fileExtension = explode("/", $value["type"])[1];
+                                if (!str_contains($allowedExtensions, $fileExtension)) {
+                                    $errors[$field] = str_replace("_", " ", $field) . "'s extension must be " . str_replace(",", " or ", $allowedExtensions);
+                                }
                             }
                         }
                     }
