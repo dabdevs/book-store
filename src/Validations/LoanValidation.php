c370a00a5ae48d2d91114667019b61282a027f8e
Alain Jean <ajean@sugit.com.ar>
2024-05-23 20:06:52 -0300

    loan validation

---
+++ src/Validations/LoanValidation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validations;
+
+class LoanValidation
+{
+    public static $rules = [
+        "table" => "loans", 
+        "book_id" => ["required", "number", "books:exists"],
+        "user_id" => ["required", "number", "users:exists", "users:member"],
+        "borrow_date" => ["required", "string"],
+        "return_date" => ["required", "string"],
+        "status" => ["required", "enum:BORROWED,RETURNED"],
+    ];
+}
