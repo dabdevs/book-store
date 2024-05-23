e0b62c2449b4b4f3db5fda429c31305365b38d0c
Alain Jean <ajean@sugit.com.ar>
2024-05-23 12:16:12 -0300

    card link

--- src/Views/dashboard/cards.inc.php
+++ src/Views/dashboard/cards.inc.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4" role="button" onclick="location.href='/books'">
         <div class="card">
             <div class="card-header p-3 pt-2">
                 <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
@@ -17,7 +17,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4" role="button" onclick="location.href='/loans'">
         <div class="card">
             <div class="card-header p-3 pt-2">
                 <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
@@ -37,7 +37,7 @@
     </div>
 
     <?php if (in_array($user->role, ["ADMIN", "LIBRERIAN"])) { ?>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4" role="button" onclick="location.href='/members'">
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
@@ -58,7 +58,7 @@
     <?php } ?>
 
     <?php if ($user->role === "ADMIN") { ?>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4" role="button" onclick="location.href='/librerians'">
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
