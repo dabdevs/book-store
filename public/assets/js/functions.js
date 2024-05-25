276972631c9fd4a1717a5ffaa2e76c22e2e69123
Alain Jean <ajean@sugit.com.ar>
2024-05-25 16:14:26 -0300

    delete custom confirm modal

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,21 +1,16 @@
 function deleteItem(title, id, action) {
-    Swal.fire({
-        title: `Do you want to delete ${title}?`,
-        showCancelButton: true,
-        confirmButtonText: "Save",
-    }).then(async (result) => {
-        if (result.isConfirmed) {
-            const form = document.getElementById("delete-form")
-            const deleteId = document.getElementById("delete-id")
+    const form = document.getElementById("delete-form")
+    const itemTitle = document.getElementById("item-title")
+    itemTitle.innerText = title
+    const deleteId = document.getElementById("delete-id")
 
-            // Set ID to delete
-            deleteId.value = id
+    // Set ID to delete
+    deleteId.value = id
 
-            // Set form action
-            form.action = action
+    // Set form action
+    form.action = action
 
-            // Submit form
-            form.submit()
-        }
-    });
+    const modal = new bootstrap.Modal("#delete-form")
+
+    modal.show()
 }
